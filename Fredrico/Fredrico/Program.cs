dfe88d2e44a5be9d703442ec82b0a75b45bd83ce
ld137 <83894641+ld137@users.noreply.github.com>
2021-09-28 12:20:43 +1300

    Add files via upload

---
+++ Fredrico/Fredrico/Program.cs
@@ -0,0 +1,66 @@
+﻿using Discord.Addons.Hosting;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Fredrico
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", false, true)
+                    .Build();
+
+                    x.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Debug);
+                })
+                .ConfigureDiscordHost((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Debug,
+                        AlwaysDownloadUsers = false,
+                        MessageCacheSize = 200,
+                    };
+
+                    config.Token = context.Configuration["Token"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = LogSeverity.Debug;
+                    config.DefaultRunMode = RunMode.Sync;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    //services
+                    //    .AddHostedService<CommandHandler>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+
+
+        }
+    }
+}
---
+++ Fredrico/Fredrico/Services/CommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fredrico.Services
+{
+    public class CommandHandler : InitializedService
+    {
+        private readonly IServiceProvider provider;
+        private readonly DiscordSocketClient client;
+        private readonly CommandService service;
+        private readonly IConfiguration configuration;
+
+        public CommandHandler(IServiceProvider provider, DiscordSocketClient client, CommandService service, IConfiguration configuration)
+        {
+            this.provider = provider;
+            this.client = client;
+            this.service = service;
+            this.configuration = configuration;
+        }
+        public override async Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            this.client.MessageReceived += OnMessageRecieved();
+        }
+
+        private async Task OnMessageRecieved(SocketMessage socketMessage)
+        {
+            if (!(socketMessage is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            var argPos = 0;
+            if (!message.HasStringPrefix(this.configuration["Prefix"], ref argPos) && !message.HasMentionPrefix(this.client.CurrentUser, ref argPos)) return;
+
+            var context = new SocketCommandContext(this.client, message);
+            await this.service.ExecuteAsync(context, argPos, this.provider);
+        }
+    }
+}
